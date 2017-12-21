569759480b4151572809e6c08f98c9a6fdf1ecbd
Mukesh Waghdhare <mukesh.waghadhare@talentica.com>
2017-12-21 14:39:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
     currentTime.setTimezone("Asia/Calcutta");
+    currentTime.setTimezone("Asia/Calcutta");
     callback(null, {
         statusCode: '200',
         body: 'The time in India is: ' + currentTime.toString(),
